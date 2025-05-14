4f1e8b180b58f85c3a3e6bd1f6982330f669d377
Benjtalkshow <chinedubenj@gmail.com>
2025-05-14 10:48:23 +0100

    chore: initial app setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Personal Finance Tracker",
+  description: "Track your income and expenses with ease",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+          <main className="min-h-screen bg-background">{children}</main>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+    return (
+        <div><h1>Nextjs 15</h1></div>
+    )
+}
